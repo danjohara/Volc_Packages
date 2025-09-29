dcddb182ea899ba8209f667a3adb9cdf69ccfd28
danjohara <35135654+danjohara@users.noreply.github.com>
2025-09-28 22:59:44 -0700

    Add files via upload

--- Examples/Example_Scripts/Test_Morvolc.m
+++ Examples/Example_Scripts/Test_Morvolc.m
@@ -5,8 +5,8 @@
 %   for the Aracar volcano.
 
 %% Script Parameters
-packagePath = '.\..\..';
-tifFolder = '.\..\Example_DEMs\Aracar_Data\';
+packagePath = './../..';
+tifFolder = './../Example_DEMs/Aracar_Data/';
 tifFile = 'Aracar_Elevation_UTM.tif';
 boundaryFile = 'Aracar_Boundary.shp';
 craterFile = 'Aracar_Crater.shp';
@@ -15,31 +15,55 @@ dx = 30;
 prefix = 'Aracar_';
 
 %% Create Package
+% Input Files
 pack.tifFile = [tifFolder,tifFile];
 pack.boundaryXY = [tifFolder,boundaryFile];
-pack.maskMap = []; % [tifFolder,maskFile]
+pack.maskXY = []; % [tifFolder,maskFile]
 pack.craterXY = [tifFolder,craterFile]; % [tifFolder,craterFile]
 
+% Map Resolution
 pack.dx = dx;
+
+% Analysis elevation intervals
 pack.peakDiff = .05;
 pack.contIter = -.05;
 pack.peakContIter = -.05;
 pack.craterContIter = -.05;
 
+% Summit analysis
 pack.correctCrater = 1;
 pack.summitRegion = 1;
 pack.ignoreSummitIndex = 1;
 
+% Basal surface interpolation
 pack.interpSurfaces.Natural = 1;
 pack.interpSurfaces.IDW = 1;
 pack.interpSurfaces.Kriging = 1;
 
-pack.xlsFile = '.\..\Example_Results\Morvolc\Test_Results.xlsx';
-pack.saveResFolder = '.\..\Example_Results\MorVolc\';
+% Topography analysis
+pack.hypsIter = .01;
+pack.roughnessWindows = [250,500,1000,2000];
+pack.roughnessType = 'tpi';
+pack.slopeVarianceWindows = [250,500,1000,2000];
 
+% Save parameters
+pack.xlsFile = 1;
+pack.saveResFolder = './../Example_Results/MorVolc/';
+pack.saveFigFolder = './../Example_Results/MorVolc/';
+pack.saveInputs = 1;
+
+% Figure parameters
 pack.plotResults = 1;
+pack.visPlots = 1;
+pack.figTitlePrefix = 'Aracar';
 pack.figPrefix = prefix;
-pack.saveFigFolder = '.\..\Example_Results\MorVolc\';
+
+% Verbose
+pack.verbose = 1;
+
+% Zipping and deleting results
+pack.zipFiles = 3;
+pack.deleteAfterZip = 0;
 
 %% Run Analysis
 addpath(genpath(packagePath))
